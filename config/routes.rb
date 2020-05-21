f1a8dccca3042f4a6e1c13e8defc0b73e730bc4a
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-05-21 14:07:47 +0900

    default system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'tweets#index'
-  resources :tweets, only: :index
+  get 'users/index'
+  devise_for :users
+  root to: 'users#index'
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  resources :tweets do
+    resources :comments, only: :create
+  end
 end

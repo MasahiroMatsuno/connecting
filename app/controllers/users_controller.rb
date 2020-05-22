696db614eab984c7e53d4567ec041690ab338ed5
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-05-22 11:19:09 +0900

    feature_user-path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @realname = current_user.realname
-    @tweets = current_user.tweets
+    @realname = @user.realname
+    @tweets = @user.tweets
   end
 end

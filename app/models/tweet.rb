2ab04d5e83bdf298d5008a75cff0081eb773a273
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-05-25 11:21:38 +0900

    画像投稿機能の実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,7 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments 
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+  mount_uploader :image, ImageUploader
 end

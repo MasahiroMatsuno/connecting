ef404d59b428e9c445ea796bc37d87291ebe19c4
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-08-18 10:59:41 +0900

    devise編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,17 @@ class UsersController < ApplicationController
     @tweets = @user.tweets
   end
 
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def new_guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
-  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  post 'follow/:id' => 'relationships#follow', as: 'follow' 
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
   resources :homes, only: :index
   resources :tweets do
     resources :comments, only: :create
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
     resources :likes, only: [:create, :destroy]
   end
-  resources :users, :only => [:index, :show]
+  resources :users
   post '/users/guest_sign_in', to: 'users#new_guest'
   resources :messages, :only => [:create]
   resources :rooms, :only => [:create, :show, :index]
